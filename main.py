4e59b987efed25743984568dc944bbc199e630d6
roopeshkp34 <roopeshkp34@gmail.com>
2023-05-10 15:47:27 +0530

    refactor:

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 
 from app.routers.v1 import api as v1_apis
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return "Crud APP"
+
+origins = [
+    '*'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change this to the domain of your frontend application
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=v1_apis.router, prefix="/v1")
